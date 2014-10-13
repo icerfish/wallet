b87edf0779b4a5f29a1aaca8e9d9d9f0a05b133f
Dylan Turney <dylan@makelight.co>
2014-10-13 23:39:48 +0100

    Updated with View Source link

--- public/javascripts/transactionController.js
+++ public/javascripts/transactionController.js
@@ -2,80 +2,129 @@
  * Created by dylanturney on 12/10/14.
  */
 
-angular.module('myApp', []).controller('transactionController', ['$scope','$http', function($scope,$http) {
-    var site = "http://localhost:3000";
-    var page = "/transactions/all";
-    $http.get(site + page)
+var app = angular.module('myApp', []).controller('transactionController', ['$scope','$http', function($scope,$http) {
+    $http.get("/transactions/all") //Gets the transactions from Server
         .success(function(response){
-            console.log("Response: " + JSON.stringify(response));
             $scope.transactions = response;
-
-            console.log("trans: " + JSON.stringify($scope.transactions));
         });
 
-    $scope.amount = 0;
+    $scope.amount = 0; //Sets amount to 0
 
-    $scope.options = [
+    $scope.options = [ //Options for Select
         {name:'Add', addition: true},
         {name:'Remove', addition: false}
     ];
 
-    $scope.selectedOption = $scope.options[0];
+    $scope.selectedOption = $scope.options[0]; //Sets current option
 
-    $scope.getTotal = function() {
+    $scope.getTotal = function() { //Grabs total from Transactions
         var total = 0;
         if(typeof $scope.transactions != 'undefined')
             for(var i = 0; i < $scope.transactions.length; i++){
                 var transaction = $scope.transactions[i];
-                if(transaction.addition){
-                    total += transaction.amount;
+                if(transaction.addition){ // Checks to see if transaction is addition of subtraction
+                    total += transaction.amount; //Adds transaction amount
                 } else {
-                    total -= transaction.amount;
+                    total -= transaction.amount; //Subtracts transaction amount
                 }
             }
-        return total;
+        return total; //Returns total
     };
 
     $scope.getMinValue = function() {
-        return 0.01;
+        return 0.01; //Set to 0.01 so value can never be 0
     };
 
     $scope.getMaxValue = function() {
-        if(!$scope.selectedOption.addition)
+        if(!$scope.selectedOption.addition) //If subtraction return total amount in wallet else return null
             return $scope.getTotal();
         else
             return null;
     };
 
-    $scope.postTransaction = function() {
+    $scope.postTransaction = function() { //Post transaction to server
 
         var dataObject = {
             amount : $scope.amount,
             addition  : $scope.selectedOption.addition
         };
 
-        console.log("Data Obj: " + JSON.stringify(dataObject));
-
         $http.post('/transactions/add', dataObject).
-            success(function(data, status, headers, config) {
+            success(function(data, status, headers, config) { //If transaction was successful add to Wallet
                 console.log("Data: " + JSON.stringify(data));
                 $scope.transactions.push(data);
             }).
             error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
+                //Server error handling typically should go here
             });
     };
 
-    $scope.resetTransactions = function(){
-        console.log('resetting transactions mofo');
+    $scope.resetTransactions = function(){ //Places
         $http.post('/transactions/reset').
             success(function(data, status, headers, config) {
-
+                //Server will redirect to home
             }).
             error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
+                //Server error handling typically should go here
+
             });
     }
 }]);
+
+//The below code is taken from Stackoverflow User: Guillaume86
+//Source: http://stackoverflow.com/a/16242433/1627272
+
+
+function isEmpty(value) {
+    return angular.isUndefined(value) || value === '' || value === null || value !== value;
+}
+
+app.directive('ngMin', function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, elem, attr, ctrl) {
+            scope.$watch(attr.ngMin, function () {
+                ctrl.$setViewValue(ctrl.$viewValue);
+            });
+            var minValidator = function (value) {
+                var min = scope.$eval(attr.ngMin) || 0;
+                if (!isEmpty(value) && value < min) {
+                    ctrl.$setValidity('ngMin', false);
+                    return undefined;
+                } else {
+                    ctrl.$setValidity('ngMin', true);
+                    return value;
+                }
+            };
+
+            ctrl.$parsers.push(minValidator);
+            ctrl.$formatters.push(minValidator);
+        }
+    };
+});
+
+app.directive('ngMax', function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, elem, attr, ctrl) {
+            scope.$watch(attr.ngMax, function () {
+                ctrl.$setViewValue(ctrl.$viewValue);
+            });
+            var maxValidator = function (value) {
+                var max = scope.$eval(attr.ngMax) || Infinity;
+                if (!isEmpty(value) && value > max) {
+                    ctrl.$setValidity('ngMax', false);
+                    return undefined;
+                } else {
+                    ctrl.$setValidity('ngMax', true);
+                    return value;
+                }
+            };
+
+            ctrl.$parsers.push(maxValidator);
+            ctrl.$formatters.push(maxValidator);
+        }
+    };
+});
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var save = require('../modules/database/save.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Wallet' });
+  res.render('index', { title: 'Wallet' }); //Render index
 });
 
 
@@ -14,7 +14,7 @@ router.get('/transactions/all', function(req, res) {
     query.QueryTransactions(database.TransactionModel, function(results){
 
         if(!results){
-            res.send(500);
+            res.send(500); //Returns 500 if error querying Mongo occurs
         } else {
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(results, null, 3));
@@ -26,7 +26,7 @@ router.get('/transactions/all', function(req, res) {
 router.post('/transactions/add', function(req, res) {
     save.SaveTransaction(database.TransactionModel, req.body, function (success, results) {
         if (!success)
-            res.send(500);
+            res.send(500); //Returns 500 if error adding to Mongo occurs
         else {
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(results, null, 3));
@@ -37,8 +37,8 @@ router.post('/transactions/add', function(req, res) {
 });
 
 router.get('/transactions/reset', function(req, res){
-    database.TransactionModel.remove({}, function(err) {
-        res.redirect('/');
+    database.TransactionModel.remove({}, function(err) { //Remove all documents in collection
+        res.redirect('/'); //Redirect user to home
     });
 });
 
